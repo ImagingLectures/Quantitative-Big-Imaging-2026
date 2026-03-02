ca1980bc24d7066a69786162cd891164e6babcf7
Anders Kaestner <anders.kaestner@psi.ch>
2026-03-02 08:31:59 +0100

    Finalizing lecture 2 and adding a lot of notes for the lecture script.

--- building/buildLecture02.sh
+++ building/buildLecture02.sh
@@ -6,5 +6,5 @@ else
     jupyter-book build Lectures/Lecture-02 --builder pdflatex
 fi
 
-gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/ebook \
+gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/printer \
    -dNOPAUSE -dQUIET -dBATCH -sOutputFile=docs/QBI-Lecture02-Datasets.pdf Lectures/Lecture-02/_build/latex/QBI-Lecture02-Datasets.pdf
